60c188ed625d202ec39db93a2546dd726a096e6d
Natnael <natnaelzed9@gmail.com>
2025-04-11 13:19:30 -0700

    Script.js

--- Script.js
+++ Script.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/*console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '.....correct number';
-document.querySelector('.score').textContent='10';
-document.querySelector('.number').textContent='13'
-
-document.querySelector('.Guess').value = 23;
-console.log(document.querySelector('.Guess').value);
-*/ 
 let secretnumber=Math.trunc(Math.random()*20)+1;
 let score=20;
 let highscore=0;
@@ -45,27 +37,7 @@ document.querySelector('.check').addEventListener
     }
 }
     
-    /*else if(guess<secretnumber){
-        if(score>1){
-        document.querySelector('.message').textContent = 'too low';
-        score--;
-        document.querySelector('.score').textContent=score;
-    }else{
-        document.querySelector('.message').textContent = 'you lose the game';
-        document.querySelector('.score').textContent=0;
-
-    }
-    }else if(guess>secretnumber){
-        if(score>1){
-        document.querySelector('.message').textContent = 'too high';
-        score--;
-        document.querySelector('.score').textContent=score;
-    }else{
-        document.querySelector('.message').textContent = 'you lose the game';
-        document.querySelector('.score').textContent=0;
-    }
-}*/
-})
+   
 document.querySelector('.btnAgain').addEventListener
 ('click',function(){
     score=20;
